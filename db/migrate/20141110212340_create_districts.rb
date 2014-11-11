b0875b42a4247ba4f1c7f4df651b831c975b2fdc
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-10 20:59:13 -0500

    Modify district migration fields to be jurisdiction name, number and borough - can add more due to info in API

--- db/migrate/20141110212340_create_districts.rb
+++ db/migrate/20141110212340_create_districts.rb
@@ -1,9 +1,9 @@
 class CreateDistricts < ActiveRecord::Migration
   def change
     create_table :districts do |t|
-      t.string :name
+      t.string :jurisdiction_name
+      t.integer :number
       t.string :borough
-      t.string :superintendent
 
       t.timestamps
     end
