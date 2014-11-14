041cf9c6b0a6eba301966679bb8b569e9cb6dd90
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-14 11:09:20 -0500

    Add district variable to index method, remove new, create, edit, update, destroy methods

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -4,61 +4,13 @@ class SchoolsController < ApplicationController
   # GET /schools
   # GET /schools.json
   def index
-    @schools = School.all
+    @district = District.find(params[:district_id])
+    @schools = @district.schools
   end
 
   # GET /schools/1
   # GET /schools/1.json
   def show
-  end
-
-  # GET /schools/new
-  def new
-    @school = School.new
-  end
-
-  # GET /schools/1/edit
-  def edit
-  end
-
-  # POST /schools
-  # POST /schools.json
-  def create
-    @school = School.new(school_params)
-
-    respond_to do |format|
-      if @school.save
-        format.html { redirect_to @school, notice: 'School was successfully created.' }
-        format.json { render :show, status: :created, location: @school }
-      else
-        format.html { render :new }
-        format.json { render json: @school.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /schools/1
-  # PATCH/PUT /schools/1.json
-  def update
-    respond_to do |format|
-      if @school.update(school_params)
-        format.html { redirect_to @school, notice: 'School was successfully updated.' }
-        format.json { render :show, status: :ok, location: @school }
-      else
-        format.html { render :edit }
-        format.json { render json: @school.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /schools/1
-  # DELETE /schools/1.json
-  def destroy
-    @school.destroy
-    respond_to do |format|
-      format.html { redirect_to schools_url, notice: 'School was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
