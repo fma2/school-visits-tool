625e8ec3cd6b439acc8f5ace2aca46f4f46cc9bb
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-14 12:42:49 -0500

    Add routes for welcome signin and user home, nest reports routes in users

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,18 @@ Rails.application.routes.draw do
   
   resources :reports
 
-  root "welcome#index"
+  root "welcome#signin"
 
-  get 'welcome/index'
+  get 'welcome/signin'
+  get 'welcome/home'
 
   devise_for :users
+
+  get '/home' => 'users#home'
+
+  resources :users do 
+    resources :reports
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
