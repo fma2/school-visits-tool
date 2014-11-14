6ba11442808e03f2504a962df3d71f01b688617a
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-14 15:37:55 -0500

    Add if-else for whether user is signed in to home method, set_user method sets user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
-	
-	def home
+	before_action :authenticate_user!, :set_user	
 
+	def home
+		if user_signed_in?
+			@user
+		else
+			redirect_to new_user_session_path
+		end 
 	end
 
+	private
+
+		def set_user
+			@user = current_user
+		end
 end
