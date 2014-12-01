7bc8bca9ab1455fe143e4c6a5ad65d12e684382c
Farheen Malik <farheen.m.malik@gmail.com>
2014-12-01 14:26:15 -0500

    Update schema based on migration for pg_search

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110220510) do
+ActiveRecord::Schema.define(version: 20141201190848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20141110220510) do
     t.string   "borough"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "pg_search_documents", force: true do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "reports", force: true do |t|
